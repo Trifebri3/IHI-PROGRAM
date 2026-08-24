692efffbe70c0216e7fb4dfc4909ad9d021cab96
TRI FEBRIANSAH <trifebriansah321@gmail.com>
2026-08-24 11:01:35 +0700

    fix: make email verification synchronous to avoid queue blockage

--- app/Notifications/QueuedVerifyEmail.php
+++ app/Notifications/QueuedVerifyEmail.php
@@ -2,17 +2,12 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Auth\Notifications\VerifyEmail as BaseVerifyEmail;
 
-// 1. Extend ke BaseVerifyEmail bawaan Laravel
-// 2. Implement ShouldQueue agar masuk ke background job
-class QueuedVerifyEmail extends BaseVerifyEmail implements ShouldQueue
+// Extend ke BaseVerifyEmail bawaan Laravel secara sinkron tanpa ShouldQueue
+// agar email verifikasi dikirim langsung saat registrasi & kendala SMTP langsung terdeteksi
+class QueuedVerifyEmail extends BaseVerifyEmail
 {
-    use Queueable;
-
     // Kita tidak perlu menulis ulang logika pengiriman email,
-    // karena sudah ditangani oleh BaseVerifyEmail.
-    // Class ini hanya berfungsi untuk menambahkan sifat "ShouldQueue".
+    // karena sudah ditangani oleh BaseVerifyEmail secara default.
 }
